9136d0f84bcacdf15a34979ec4ce6bdefbb4fc79
AleksBab-star <aleksandrbabikov8@gmail.com>
2026-03-25 17:18:44 +0300

    Создан компонент Hero страницы Services

---
+++ src/pages/Services/Hero.tsx
@@ -0,0 +1,7 @@
+export default function Hero(){
+    return(
+        <section className="h-100 bg-blue-300 flex items-center justify-center">
+            <h1>Проверка</h1>
+        </section>
+    )
+}
