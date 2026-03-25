42f3e74a234089e15e1ed40425347a9ad587656d
Artem Tarakanov <artemtarakanov2505@gmail.com>
2026-03-25 16:55:28 +0300

    Добавлена страница Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-
+import { Routes, Route } from 'react-router-dom';
 import './App.css'
+import Home from './pages/Home.tsx';
 
 function App() {
 
   return (
     <>
-
+      <Routes>
+        <Route path="/" element={<Home/>} />
+      </Routes>
     </>
   )
 }
