a0e43cf1359a4c63b384402cf4780def70d5b11e
Artem Tarakanov <artemtarakanov2505@gmail.com>
2026-03-26 17:14:52 +0300

    Добавлен компонент Hero страницы Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,11 @@
 import Header from "../components/Header.tsx";
+import Hero from "./Home/Hero.tsx";
 export default function Home() {
     return (
         <div>
 
             <Header />
-
+            <Hero/>
         </div>
     );
 }
