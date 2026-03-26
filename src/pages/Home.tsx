6f2768bec6a09afc16b39f1ef4261d86de596f32
Artem Tarakanov <artemtarakanov2505@gmail.com>
2026-03-26 14:27:12 +0300

    Очищена тестовая секция

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,9 @@
 import Hero from "./Home/Hero.tsx";
+import Header from "../components/Header.tsx"
 export default function Home() {
     return (
         <div>
-
+            <Header/>
             <Hero />
 
         </div>
