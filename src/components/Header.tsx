31a2c49f568a555f50397658770e5f0656b2fcac
Artem Tarakanov <artemtarakanov2505@gmail.com>
2026-03-26 13:08:31 +0300

    Восстановил Header и Hero после конфликта со stash

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+export default function Header(){
+    return(
+        <section className = "bg-[#0F0F0F] flex px-10 py-7.5 h-full ">
+
+            <div className = "bg-[#191919] flex-row items-center justify-between px-8 py-5">
+
+            </div>
+
+        </section>
+    )
+}
