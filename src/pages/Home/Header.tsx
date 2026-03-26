a2c11900fb41a32450325b24de71efaf7251edba
Artem Tarakanov <artemtarakanov2505@gmail.com>
2026-03-26 13:09:53 +0300

    Удален ненужный файл из страницы Home

--- src/pages/Home/Header.tsx
+++
@@ -1,7 +0,0 @@
-export default function Header(){
-    return (
-        <section className = "h-20 bg-red-600 flex items-center justify-center">
-            <h1>Проверка</h1>
-        </section>
-    );
-}
