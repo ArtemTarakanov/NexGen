b9c664bb4dcb6618af8a148033109b69c0db8f65
Artem Tarakanov <artemtarakanov2505@gmail.com>
2026-03-27 14:39:58 +0300

    Добавлен компонент Reasons в страницу Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import Header from "../components/Header.tsx";
 import Hero from "./Home/Hero.tsx";
+import Reasons from "./Home/Reasons.tsx";
 export default function Home() {
     return (
         <div>
 
             <Header />
             <Hero/>
+            <Reasons/>
         </div>
     );
 }
