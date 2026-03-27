d641dc7457a13bbfc23dd79ddb121b5248273e5f
Artem Tarakanov <artemtarakanov2505@gmail.com>
2026-03-27 16:15:12 +0300

    Реализован компонент Services

---
+++ src/pages/Home/Services.tsx
@@ -0,0 +1,84 @@
+export default function Services(){
+
+    const cards = [
+        {
+            icon: (<svg width="56" height="56" viewBox="0 0 56 56" fill="none" xmlns="http://www.w3.org/2000/svg">' + '<rect x="0.5" y="0.5" width="55" height="55" rx="9.5" fill="#1F1F1F"/>' + '<rect x="0.5" y="0.5" width="55" height="55" rx="9.5" stroke="#333333"/>' + '<path d="M18.7195 20.4069L20.4069 18.7195C21.9977 17.1287 22.7931 16.3333 23.7816 16.3333C24.77 16.3333 25.5654 17.1287 27.1562 18.7195L24.344 21.5318L18.7195 27.1562L18.7195 27.1562C17.1287 25.5654 16.3333 24.77 16.3333 23.7816C16.3333 22.7931 17.1287 21.9977 18.7195 20.4069L18.7195 20.4069Z" fill="#CE7D63"/>' + '<path d="M37.2803 35.593L35.593 37.2803C34.0021 38.8712 33.2067 39.6666 32.2183 39.6666C31.2299 39.6666 30.4344 38.8712 28.8436 37.2803L28.8436 37.2803L34.4681 31.6558L37.2803 28.8436C38.8712 30.4344 39.6666 31.2299 39.6666 32.2183C39.6666 33.2067 38.8712 34.0021 37.2803 35.593Z" fill="#CE7D63"/>' + '<path d="M25.9188 36.4875C26.29 36.198 26.6267 35.8613 27.3 35.188L35.9285 26.5595C34.7542 26.0707 33.3633 25.2678 32.0478 23.9524C30.7322 22.6367 29.9293 21.2456 29.4405 20.0711L20.8119 28.6998L20.8118 28.6999C20.1385 29.3732 19.8018 29.7098 19.5123 30.0811C19.1707 30.519 18.8779 30.9928 18.639 31.4941C18.4364 31.9191 18.2859 32.3708 17.9847 33.2742L16.3968 38.0381C16.2486 38.4827 16.3643 38.9728 16.6957 39.3042C17.027 39.6355 17.5172 39.7513 17.9617 39.6031L22.7256 38.0151C23.629 37.714 24.0807 37.5634 24.5057 37.3609C25.0071 37.1219 25.4809 36.8291 25.9188 36.4875Z" fill="#CE7D63"/>' + '<path d="M38.3228 24.1651C40.1145 22.3735 40.1145 19.4686 38.3228 17.677C36.5312 15.8853 33.6263 15.8853 31.8347 17.677L30.7998 18.7119C30.814 18.7547 30.8287 18.798 30.8439 18.842C31.2232 19.9353 31.9389 21.3686 33.2853 22.7149C34.6316 24.0613 36.0649 24.777 37.1582 25.1563C37.202 25.1715 37.2452 25.1861 37.2878 25.2002L38.3228 24.1651Z" fill="#CE7D63"/>' + '</svg>),
+            title: 'Web Design',
+            description: 'Our Web Design service is all about creating visually stunning and user-friendly websites that leave a lasting.',
+            coast: 'Starts From $1,500'
+        },
+        {
+            icon: (<svg width="56" height="56" viewBox="0 0 56 56" fill="none" xmlns="http://www.w3.org/2000/svg">' + '<rect x="0.5" y="0.5" width="55" height="55" rx="9.5" fill="#1F1F1F"/>' + '<rect x="0.5" y="0.5" width="55" height="55" rx="9.5" stroke="#333333"/>' + '<path fill-rule="evenodd" clip-rule="evenodd" d="M20.0333 17.7001C18.6665 19.0669 18.6665 21.2668 18.6665 25.6666V30.3333C18.6665 34.733 18.6665 36.9329 20.0333 38.2998C21.4002 39.6666 23.6001 39.6666 27.9998 39.6666C32.3996 39.6666 34.5995 39.6666 35.9663 38.2998C37.3332 36.9329 37.3332 34.733 37.3332 30.3333V25.6666C37.3332 21.2668 37.3332 19.0669 35.9663 17.7001C34.5995 16.3333 32.3996 16.3333 27.9998 16.3333C23.6001 16.3333 21.4002 16.3333 20.0333 17.7001ZM24.4998 18.9583C24.0166 18.9583 23.6248 19.35 23.6248 19.8333C23.6248 20.3165 24.0166 20.7083 24.4998 20.7083H31.4998C31.9831 20.7083 32.3748 20.3165 32.3748 19.8333C32.3748 19.35 31.9831 18.9583 31.4998 18.9583H24.4998ZM27.9998 36.1666C29.2885 36.1666 30.3332 35.1219 30.3332 33.8333C30.3332 32.5446 29.2885 31.4999 27.9998 31.4999C26.7112 31.4999 25.6665 32.5446 25.6665 33.8333C25.6665 35.1219 26.7112 36.1666 27.9998 36.1666Z" fill="#CE7D63"/>' + '</svg>),
+            title: 'Mobile App Development',
+            description: 'With our Mobile App Development service, we harness the power of mobile technology to create cutting-edge applications .',
+            coast: 'Starts From $2,500'
+        },
+        {
+            icon: (<svg width="56" height="56" viewBox="0 0 56 56" fill="none" xmlns="http://www.w3.org/2000/svg">' + '<rect x="0.5" y="0.5" width="55" height="55" rx="9.5" fill="#1F1F1F"/>' + '<rect x="0.5" y="0.5" width="55" height="55" rx="9.5" stroke="#333333"/>' + '<path fill-rule="evenodd" clip-rule="evenodd" d="M39.6666 28C39.6666 34.4434 34.4432 39.6667 27.9999 39.6667C21.5566 39.6667 16.3333 34.4434 16.3333 28C16.3333 21.5567 21.5566 16.3334 27.9999 16.3334C34.4432 16.3334 39.6666 21.5567 39.6666 28ZM29.736 21.5203C30.2028 21.6453 30.4798 22.1251 30.3547 22.5919L27.3352 33.8611C27.2101 34.3278 26.7303 34.6048 26.2635 34.4798C25.7967 34.3547 25.5197 33.8749 25.6448 33.4081L28.6643 22.139C28.7894 21.6722 29.2692 21.3952 29.736 21.5203ZM31.4645 23.8813C31.8062 23.5396 32.3603 23.5396 32.702 23.8813L32.945 24.1244C33.6863 24.8656 34.3044 25.4836 34.7291 26.0403C35.1773 26.6278 35.4918 27.2486 35.4918 28C35.4918 28.7515 35.1773 29.3723 34.7291 29.9598C34.3044 30.5165 33.6863 31.1345 32.9451 31.8757L32.702 32.1188C32.3603 32.4605 31.8062 32.4605 31.4645 32.1188C31.1228 31.7771 31.1228 31.223 31.4645 30.8813L31.6647 30.6812C32.46 29.8859 32.9929 29.3504 33.3378 28.8983C33.6673 28.4664 33.7418 28.2151 33.7418 28C33.7418 27.785 33.6673 27.5337 33.3378 27.1018C32.9929 26.6497 32.46 26.1142 31.6647 25.3189L31.4645 25.1188C31.1228 24.7771 31.1228 24.223 31.4645 23.8813ZM23.2981 23.8813C23.6398 23.5396 24.1938 23.5396 24.5355 23.8813C24.8772 24.223 24.8772 24.7771 24.5355 25.1188L24.3354 25.3189C23.5401 26.1142 23.0072 26.6497 22.6623 27.1018C22.3328 27.5337 22.2583 27.785 22.2583 28C22.2583 28.2151 22.3328 28.4664 22.6623 28.8983C23.0072 29.3504 23.5401 29.8859 24.3354 30.6812L24.5355 30.8813C24.8772 31.223 24.8772 31.7771 24.5355 32.1188C24.1938 32.4605 23.6398 32.4605 23.2981 32.1188L23.055 31.8757C22.3138 31.1345 21.6957 30.5165 21.271 29.9598C20.8227 29.3723 20.5083 28.7515 20.5083 28C20.5083 27.2486 20.8227 26.6278 21.271 26.0403C21.6957 25.4836 22.3138 24.8656 23.055 24.1244L23.2981 23.8813Z" fill="#CE7D63"/>' + '</svg>),
+            title: 'Web Development',
+            description: 'Our Web Development service is focused on turning your website into a powerful digital asset. We utilize the latest technologies and industry best practices to build websites.',
+            coast: 'Starts From $1,800'
+        },
+        {
+            icon: (<svg width="56" height="56" viewBox="0 0 56 56" fill="none" xmlns="http://www.w3.org/2000/svg">' + '<rect x="0.5" y="0.5" width="55" height="55" rx="9.5" fill="#1F1F1F"/>' + '<rect x="0.5" y="0.5" width="55" height="55" rx="9.5" stroke="#333333"/>' + '<path d="M16.3335 21.5834C16.3335 18.6839 18.684 16.3334 21.5835 16.3334C24.483 16.3334 26.8335 18.6839 26.8335 21.5834V25.0834C26.8335 25.4902 26.8335 25.6937 26.7888 25.8606C26.6674 26.3135 26.3136 26.6673 25.8607 26.7887C25.6938 26.8334 25.4904 26.8334 25.0835 26.8334H21.5835C18.684 26.8334 16.3335 24.4829 16.3335 21.5834Z" fill="#CE7D63"/>' + '<path d="M29.1668 30.9167C29.1668 30.5098 29.1668 30.3064 29.2116 30.1395C29.3329 29.6866 29.6867 29.3328 30.1396 29.2114C30.3065 29.1667 30.51 29.1667 30.9168 29.1667H34.4168C37.3163 29.1667 39.6668 31.5172 39.6668 34.4167C39.6668 37.3162 37.3163 39.6667 34.4168 39.6667C31.5173 39.6667 29.1668 37.3162 29.1668 34.4167V30.9167Z" fill="#CE7D63"/>' + '<path d="M16.3335 34.4167C16.3335 31.5172 18.684 29.1667 21.5835 29.1667H24.7335C25.4686 29.1667 25.8361 29.1667 26.1169 29.3098C26.3638 29.4356 26.5646 29.6364 26.6904 29.8833C26.8335 30.1641 26.8335 30.5316 26.8335 31.2667V34.4167C26.8335 37.3162 24.483 39.6667 21.5835 39.6667C18.684 39.6667 16.3335 37.3162 16.3335 34.4167Z" fill="#CE7D63"/>' + '<path d="M29.1668 21.5834C29.1668 18.6839 31.5173 16.3334 34.4168 16.3334C37.3163 16.3334 39.6668 18.6839 39.6668 21.5834C39.6668 24.4829 37.3163 26.8334 34.4168 26.8334H30.6668C30.4927 26.8334 30.4056 26.8334 30.3324 26.8251C29.7239 26.7566 29.2436 26.2763 29.1751 25.6678C29.1668 25.5946 29.1668 25.5075 29.1668 25.3334V21.5834Z" fill="#CE7D63"/>' + '</svg>),
+            title: 'Digital Marketing',
+            description: 'In the digital age, marketing is a critical aspect of your business\'s success. Our Digital Marketing service employs data-driven strategies to enhance your brand\'s visibility',
+            coast: 'Starts From $1,200'
+        },
+    ]
+
+    return(
+        <section className = "h-full py-10 max-md:px-4 max-md:py-3 px-10 bg-[#0F0F0F]">
+
+            {/*Main*/}
+            <div className="flex flex-col border-2 border-[#1F1F1F] p-2.5 rounded-[20px] gap-2.5">
+
+                {/*secondary*/}
+                <div className="bg-[#191919] p-10 max-md:p-6 rounded-[14px]">
+                    <h2 className = "font-['Roboto_Flex'] font-semibold text-[38px] max-md:text-[28px] leading-[100%] tracking-[0%] uppercase text-[#F9EFEC]">Our Services</h2>
+                </div>
+
+                <div className = "max-md:overflow-x-auto w-full">
+                    <div className = " w-full grid grid-cols-2 gap-2.5 max-md:flex max-md:w-max max-md:gap-x-6">
+                        {cards.map((item, index) => (
+                            <div key={index} className="bg-[#191919] p-15 max-md:pt-6 max-md:px-7 max-md:pb-6 rounded-[14px] flex flex-col gap-8 max-md:gap-5 max-md:shrink-0">
+                                <div className = "flex flex-row w-full">
+                                    <span className = "flex items-center w-full justify-between ">
+
+                                        <span className="flex items-center gap-2.5">
+                                            <span>{item.icon}</span>
+                                            <h2 className="font-['Roboto_Flex'] font-medium text-[22px] max-md:text-[20px] leading-[150%] tracking-[0%] uppercase text-[#F9EFEC]">{item.title}</h2>
+                                        </span>
+
+                                        <span className="flex items-center gap-2 max-md:hidden">
+                                            <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <rect x="0.5" y="0.5" width="39" height="39" rx="19.5" fill="#1F1F1F"/>
+                                            <rect x="0.5" y="0.5" width="39" height="39" rx="19.5" stroke="#333333"/>
+                                            <path fill-rule="evenodd" clip-rule="evenodd" d="M16.875 13.125L26.25 13.125C26.4158 13.125 26.5747 13.1908 26.6919 13.3081C26.8092 13.4253 26.875 13.5842 26.875 13.75V23.125C26.875 23.4702 26.5952 23.75 26.25 23.75C25.9048 23.75 25.625 23.4702 25.625 23.125V15.2589L14.1919 26.6919C13.9479 26.936 13.5521 26.936 13.3081 26.6919C13.064 26.4479 13.064 26.0521 13.3081 25.8081L24.7411 14.375L16.875 14.375C16.5298 14.375 16.25 14.0952 16.25 13.75C16.25 13.4048 16.5298 13.125 16.875 13.125Z" fill="#E7BEB1"/>
+                                            </svg>
+
+                                            <p className="font-['Roboto_Mono'] font-medium text-base leading-[150%] tracking-[0%] uppercase text-[#B3B3B2]">Book A Call</p>
+
+                                        </span>
+                                    </span>
+                                </div>
+
+
+                                <span className = "flex flex-col gap-4 max-md:gap-1.5">
+                                    <p className = "font-['Roboto_Flex'] font-normal text-base max-md:text-sm leading-[150%] tracking-[0%] text-[#B3B3B2] max-w-130 max-md:max-w-70 max-md:order-1">{item.description}</p>
+
+                                    <p className = "font-['Roboto_Flex'] font-medium text-[22px] max-md:text-[18px] leading-[150%] text-right max-md:text-left uppercase text-[#F9EFEC]">{item.coast}</p>
+                                </span>
+
+                                <button className = "md:hidden py-3 rounded-lg bg-[#CE7D63]">
+                                    <a href="#" className = "font-['Roboto_Flex'] font-medium text-sm leading-[150%] tracking-[0%] text-center uppercase text-[#0F0F0F]">Book A Call</a>
+                                </button>
+                            </div>
+                            ))}
+                    </div>
+                </div>
+            </div>
+
+        </section>
+    )
+}
